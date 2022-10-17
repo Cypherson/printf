ba2901f19391cfc0a8252c916f67d3aaeb714c9b
Chikendu Hillary <cikenduhill@mail.com>
2022-10-17 04:31:29 -0700

    printf project

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct print - struct for printer functions
+ * @type_arg: argument identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a printer function.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+/* For printf */
+int _printf(const char *format, ...);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
+
+#endif
