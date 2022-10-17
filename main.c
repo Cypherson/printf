c5274f0fd733351b1ad16b63e509bc88f11ea2c4
Chikendu Hillary <cikenduhill@mail.com>
2022-10-17 08:05:46 -0700

    added bnr, chr, hex, int, oct, str, unt print functionalities

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 int main(void)
 {
 	int len;
-	int len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
+	len = _printf("Hey Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
+	_printf("lenght:[%d, %i]\n", len, len);
+	_printf("Negative:[%d]\n", -762534);
 
 	return (0);
 }
